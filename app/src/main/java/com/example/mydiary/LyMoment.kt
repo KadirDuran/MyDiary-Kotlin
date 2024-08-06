92b2708d37aa213754ce54a764c4b10728e0cdd9
KadirDuran <kadirduran61@gmail.com>
2024-08-06 21:23:11 +0300

    Görsel yüklemek için gerekli izinlerin tanımlanması ve çalıştırılması eklendi.

--- app/src/main/java/com/example/mydiary/LyMoment.kt
+++ app/src/main/java/com/example/mydiary/LyMoment.kt
@@ -1,22 +1,43 @@
 package com.example.mydiary
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.media.Image
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.mydiary.databinding.FragmentLyMemoriesBinding
 import com.example.mydiary.databinding.FragmentLyMomentBinding
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.selects.select
 
 class LyMoment : Fragment() {
     private var _binding: FragmentLyMomentBinding? = null
     private val binding get() = _binding!!
+    private  lateinit var permissionLauncher : ActivityResultLauncher<String>
+    private  lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private var  selectImg: Uri?=null //URL
+    private var  selectBitmap: Bitmap?=null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        registerLauncher()
     }
 
     override fun onCreateView(
@@ -47,7 +68,92 @@ class LyMoment : Fragment() {
     }
     fun SelectImage(view: View)
     {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+        {
+            if(ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED)
+            {
+                if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),Manifest.permission.READ_MEDIA_IMAGES))
+                {
+                    Snackbar.make(view,"Görsel yüklemek için izin vermeniz gerekmektedir.",Snackbar.LENGTH_INDEFINITE).setAction(
+                        "İzin Ver",View.OnClickListener {
+                            permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                        }
+                    ).show()
+                }else
+                {
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                }
+            }else
+            {
+                val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intent)
+            }
 
+        }else
+        {
+        if(ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),Manifest.permission.READ_EXTERNAL_STORAGE))
+            {
+                Snackbar.make(view,"Görsel yüklemek için izin vermeniz gerekmektedir.",Snackbar.LENGTH_INDEFINITE).setAction(
+                    "İzin Ver",View.OnClickListener {
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    }
+                ).show()
+            }else
+            {
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }else
+        {
+            val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            activityResultLauncher.launch(intent)
+        }
+        }
+    }
+    fun registerLauncher()
+    {
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        {
+            if(it.resultCode == AppCompatActivity.RESULT_OK)
+            {
+               val intentData =  it.data
+                if(intentData != null)
+                {
+                    selectImg = intentData.data
+
+                    try {
+                        if(Build.VERSION.SDK_INT>=28)
+                        {
+                            val source = ImageDecoder.createSource(requireActivity().contentResolver,selectImg!!)
+                            selectBitmap = ImageDecoder.decodeBitmap(source)
+                            binding.imgMoment.setImageBitmap(selectBitmap)
+
+                        }else
+                        {
+                            selectBitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, selectImg)
+                            binding.imgMoment.setImageBitmap(selectBitmap)
+                        }
+                    }catch (e : Exception)
+                    {
+                        println(e.localizedMessage)
+                    }
+
+                }
+            }
+        }
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission())
+        {
+          if(it)
+            {
+              val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intent)
+            }else
+          {
+             Toast.makeText(requireContext(),"İzin verilmedi.",Toast.LENGTH_LONG).show()
+
+          }
+        }
     }
     fun Save(view: View)
     {
